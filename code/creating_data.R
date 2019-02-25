3f1df78245427b051611a7e8f080d654d6bf3607
Rob Marty <ramarty@email.wm.edu>
2019-02-24 23:04:24 -0500

    data update

--- code/creating_data.R
+++ code/creating_data.R
@@ -1,3 +1,4 @@
 df <- data.frame(var1 = 1:100,
-           var2 = runif(100))
+           var2 = runif(100),
+           var3 = runif(100))
 write.csv(df, "~/Desktop/sandbox/data/data.csv", row.names=F)
